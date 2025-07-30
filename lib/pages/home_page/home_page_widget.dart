ffb0acc7aac2d537fbdaeb929b057cf4b69bd1fa
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2025-07-30 11:04:31 -0700

    Update home_page_widget.dart

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -73,7 +73,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Nanochat',
+            'Nanochat from GitHub',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   font: GoogleFonts.interTight(
                     fontWeight:
